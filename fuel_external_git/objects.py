5d6c16980c8415a3a822f3215d6c79b6f97966d7
Ukov Dmitry <dukov@mirantis.com>
2016-08-23 13:41:11 +0300

    Fixed bug with Git Repo access via ssh
    Change-Id: I5c7dee4920ee652a67f42ecba5acbc8857246ddb

--- fuel_external_git/objects.py
+++ fuel_external_git/objects.py
@@ -7,7 +7,7 @@ from nailgun.objects.serializers.base import BasicSerializer
 from git import Repo
 from git import exc
 
-from fuel_external_git.models import GitRepo 
+from fuel_external_git.models import GitRepo
 from fuel_external_git import const
 
 class GitRepoSerializer(BasicSerializer):
@@ -36,28 +36,52 @@ class GitRepo(NailgunObject):
                 # TODO(dukov) Put some logging here
                 instance.repo = GitRepo.clone(instance.git_url)
         return instance
-           
-    
+
     @classmethod
     def create(self, data):
         if not os.path.exists(const.REPOS_DIR):
             os.mkdir(const.REPOS_DIR)
         repo_path = os.path.join(const.REPOS_DIR, data['repo_name'])
         if os.path.exists(repo_path):
+            # TODO(dukov) add some logging here
             shutil.rmtree(repo_path)
+
+        self._create_key_file(data['repo_name'], data['key'])
+        os.environ['GIT_SSH_COMMAND'] = self._get_ssh_cmd(data['repo_name'])
         repo = Repo.clone_from(data['git_url'], repo_path)
+
         instance = super(GitRepo, self).create(data)
         instance.repo = repo
         return instance
 
     @classmethod
     def checkout(self, instance):
-        commit = instance.repo.remotes.origin.fetch(refspec=instance.ref)[0].commit
-        instance.repo.head.reference = commit
-        instance.repo.head.reset(index=True, working_tree=True)
-        
-    def remove_repo(self):
-        pass
+        ssh_cmd = self._get_ssh_cmd(instance.repo_name)
+
+        if not os.path.exists(self._get_key_path(instance.repo_name)):
+            # TODO(dukov) put some logging here
+            self._create_key_file(instance.repo_name)
+
+        with instance.repo.git.custom_environment(GIT_SSH_COMMAND=ssh_cmd):
+            commit = instance.repo.remotes.origin.fetch(refspec=instance.ref)[0].commit
+            instance.repo.head.reference = commit
+            instance.repo.head.reset(index=True, working_tree=True)
+
+    @classmethod
+    def _create_key_file(self, repo_name, data):
+        key_path = self._get_key_path(repo_name)
+        with open(key_path, 'w') as key_file:
+            key_file.write(data)
+        os.chmod(key_path, 0o600)
+
+    @classmethod
+    def _get_key_path(self, repo_name):
+        return os.path.join(const.REPOS_DIR, repo_name + '.key')
+
+    @classmethod
+    def _get_ssh_cmd(self, repo_name):
+        key_path = self._get_key_path(repo_name)
+        return 'ssh -o StrictHostKeyChecking=no -i ' + key_path
 
 class GitRepoCollection(NailgunCollection):
     single = GitRepo
