e88d1e9239ea2ec27be966c4e3c5fc5052345612
Dmitry Ukov <dukov@mirantis.com>
2016-11-10 16:19:56 +0300

    Resources moved to top level of configuration hash
    Change-Id: I6884d0eff2c247dd75f76c4d39d7406249201c90

--- fuel_external_git/tests/test_openstack_config.py
+++ fuel_external_git/tests/test_openstack_config.py
@@ -20,12 +20,14 @@ class TestOpenStackConfig(base.TestCase):
 
     def test_dict_generation(self):
         resource = {
-            'DEFAULT/global_param': {
-                'value': 'global_param'
-            },
-            'DEFAULT/global_param_to_override': {
-                'value': 'global_param_to_override'
-            },
+            'nova_config': {
+                'DEFAULT/global_param': {
+                    'value': 'global_param'
+                },
+                'DEFAULT/global_param_to_override': {
+                    'value': 'global_param_to_override'
+                },
+            }
         }
         file_name = os.path.join(self.cfg_sample_dir, 'nova.conf')
         cfg = OpenStackConfig(file_name, self.config['resource_mapping'])
